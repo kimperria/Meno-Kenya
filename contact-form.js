7fd294510a13aec08ba33a661770b73715a17714
John-Kimani <kimwaweru100@gmail.com>
2022-01-20 00:35:21 +0300

    alerts feedback has been received message

--- contact-form.js
+++ contact-form.js
@@ -3,7 +3,7 @@ const clientName = document.getElementById('client-name')
 const clientLocation = document.getElementById('client-location')
 const clientEmail = document.getElementById('exampleInputEmail1')
 const nameError = document.getElementById('name-error')
-const locationError = document.getElementById('location-error')
+// const locationError = document.getElementById('location-error')
 const emailError = document.getElementById('email-error')
 
 
@@ -14,8 +14,15 @@ form.addEventListener('submit', (e) => {
     if(clientName.value === null || clientName.value == ' '){
         nameProblems.push("Name is required!")
     }
-    if(nameProblems.length > 0 || emailProblems >0){
+    if(clientEmail.value === null || clientEmail.value == ' '){
+        emailProblems.push("Email is required!")
+    }
+    if(nameProblems.length > 0 || emailProblems.length > 0){
         e.preventDefault()
         nameError.innerText = nameProblems.join(',')
+        emailError.innerText = emailProblems.join(',')
+    }
+    else{
+        alert('Hello ' + clientName.value + ' !' + 'Thank you for considering and contacting us.We shall get back you as soon as possible..' )
     }
 })
