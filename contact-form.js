68f79e0d56e558bf6d87f78b94f6b6a77510a853
John-Kimani <kimwaweru100@gmail.com>
2022-01-20 00:07:50 +0300

    assign id to relevant html elements and declare constant variables

---
+++ contact-form.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('form')
+const clientName = document.getElementById('client-name')
+const clientLocation = document.getElementById('client-location')
+const clientEmail = document.getElementById('exampleInputEmail1')
+const nameError = document.getElementById('name-error')
+const locationError = document.getElementById('location-error')
+const emailError = document.getElementById('email-error')
