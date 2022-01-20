d90b7c009b11e44a552aca80484b5e13e9d1e4e4
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-01-20 08:29:53 +0300

    Added form validation and added thanks.css, appointment.js thanks.html and thank-bg.jpg

---
+++ js/appointment.js
@@ -0,0 +1,8 @@
+let name = document.getElementById('name');
+let submit = document.getElementById('submit');
+let thank_you_name = document.getElementsByClassName('main-thanks-section-h1');
+submit.addEventListener('submit', alertTest);
+
+function alertTest() {
+    alert("hello")
+};
